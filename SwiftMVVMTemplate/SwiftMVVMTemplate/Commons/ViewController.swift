6c25fe5452958f2992ea834c594d189732d0e8a4
ME294 <dung-ht@users.noreply.github.com>
2021-01-14 16:01:34 +0700

    Implement
    [Implement] Code base

---
+++ SwiftMVVMTemplate/SwiftMVVMTemplate/Commons/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SwiftMVVMTemplate
+//
+//  Created by m2 on 1/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
